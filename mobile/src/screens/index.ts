d3dab2178e609e68266312a20b61810328dbbb74
dep <dannypeck@gmail.com>
2026-04-18 06:31:19 -0400

    fuck mobile

--- mobile/src/screens/index.ts
+++
@@ -1,6 +0,0 @@
-export { HomeScreen } from './HomeScreen';
-export { SettingsScreen } from './SettingsScreen';
-export { OnboardingScreen } from './OnboardingScreen';
-export { CloneRepositoryScreen } from './CloneRepositoryScreen';
-export { EditorScreen } from './EditorScreen';
-export { GitHubTokenScreen } from './GitHubTokenScreen';
--- mobile/src/theme/index.ts
+++
@@ -1,2 +0,0 @@
-export { ThemeProvider, useTheme, lightTheme, darkTheme } from './ThemeContext';
-export type { Theme, ThemeColors } from './ThemeContext';
